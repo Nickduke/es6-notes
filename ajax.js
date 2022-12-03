953e7a69e108fd90a1b8047d7c183a497822e069
Nick <ntoukas.nick@gmail.com>
2022-12-02 23:59:05 -0500

    AJAX - fetch, try/catch

--- ajax.js
+++ ajax.js
@@ -1,31 +1,93 @@
-// XMLHttpRequest() Method and JSON
-const btn = document.querySelector('.btn');
+// // XMLHttpRequest() Method and JSON
+// const btn = document.querySelector('.btn');
+// const url = './api/people.json';
+// btn.addEventListener('click', () => getData(url));
+
+// function getData(url) {
+//   const xhr = new XMLHttpRequest();
+//   // console.log(xhr);
+
+//   xhr.open('GET', url);
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//       const data = JSON.parse(xhr.responseText);
+//       const displayData = data
+//         .map((item) => {
+//           return `<p>${item.name}</p>`;
+//         })
+//         .join('');
+//       const element = document.createElement('div');
+//       element.innerHTML = displayData;
+//       document.body.appendChild(element);
+//     } else {
+//       console.log({ status: xhr.status, text: xhr.statusText });
+//     }
+//   };
+//   xhr.send();
+// }
+
+// Fetch Method - Promise Based
 const url = './api/people.json';
-btn.addEventListener('click', () => getData(url));
 
-function getData(url) {
-  const xhr = new XMLHttpRequest();
-  // console.log(xhr);
+// fetch(url)
+//   .then((resp) => {
+//     // convert response object into JSON data
+//     // returns promise, need to chin on another .then()
+//     return resp.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-  xhr.open('GET', url);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      const displayData = data
-        .map((item) => {
-          return `<p>${item.name}</p>`;
-        })
-        .join('');
-      const element = document.createElement('div');
-      element.innerHTML = displayData;
-      document.body.appendChild(element);
-    } else {
-      console.log({ status: xhr.status, text: xhr.statusText });
-    }
-  };
-  xhr.send();
-}
+// // fetch with function
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', () => {
+//   fetch(url)
+//     .then((resp) => resp.json())
+//     .then((data) => {
+//       displayItems(data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
-// Fetch Method
+// const displayItems = (items) => {
+//   const displayData = items
+//     .map((item) => {
+//       const { name } = item;
+//       return `<p>${name}</p>`;
+//     })
+//     .join('');
+//   const el = document.createElement('div');
+//   el.innerHTML = displayData;
+//   document.body.appendChild(el);
+// };
 
-// Try/Catch Method
+// fetch with async await
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', async () => {
+  // try/catch for error handling
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayItems(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const displayItems = (items) => {
+  const displayData = items
+    .map((item) => {
+      const { name } = item;
+      return `<p>${name}</p>`;
+    })
+    .join('');
+  const el = document.createElement('div');
+  el.innerHTML = displayData;
+  document.body.appendChild(el);
+};
